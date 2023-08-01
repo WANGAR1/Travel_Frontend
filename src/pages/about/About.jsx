b517e3e90b5cd172e05a4ab7aa0b50d138dd71ec
Najma <najmathande@gmail.com>
2023-08-01 21:59:41 +0300

    added about page

---
+++ src/pages/about/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+const About = () => {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>
+        Welcome to TravelTrove! We are a community-driven travel website
+        where you can explore various travel destinations,
+        share your travel experiences, and connect with fellow travel enthusiasts.
+      </p>
+      <p>
+        Our mission is to inspire people to travel, experience new cultures, and create unforgettable memories.
+        Whether you are a seasoned traveler or planning your first trip, TravelTrove is here to provide you with
+        valuable travel information and resources.
+      </p>
+      <p>
+        Join our community today and start discovering the
+        world one bug at a time!
+      </p>
+    </div>
+  );
+};
+export default About;
