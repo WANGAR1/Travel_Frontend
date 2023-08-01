4e8d0954c9e1bf109991137636a6dae7ac40ebd2
Najma <najmathande@gmail.com>
2023-08-01 21:58:26 +0300

    added login,signup and route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+import About from './pages/about/About';
+import Destination from './pages/destination/Destination';
+import Packages from './pages/packages/Packages';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-     
+    <div>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/destination" element={<Destination />} />
+        <Route path="/packages" element={<Packages />} />
+        {/* Add a 404 route if no match is found */}
+        <Route path="*" element={<h1>404 - Not Found</h1>} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,16 +1,10 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const Signup = () => {
-  const [isLogin, setIsLogin] = useState(false); // State to track if it's a login or signup page
-  const [username, setUsername] = useState('');
+const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
-  };
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
@@ -22,49 +16,34 @@ const Signup = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Perform any signup logic here (e.g., calling an API to register the user)
+
+    // Perform any authentication logic here (e.g., calling an API to validate user credentials)
     // For this example, we will simulate an error for demonstration purposes
-    if (username === '') {
-      setError('Username is required.');
-    } else if (email === '') {
+    if (email === '') {
       setError('Email is required.');
     } else if (password === '') {
       setError('Password is required.');
     } else {
       setError(''); // Clear any previous error
-      // Continue with the signup logic, e.g., calling an API to register the user
-      console.log('Username:', username);
+
+      // Continue with the authentication logic, e.g., calling an API to validate user credentials
       console.log('Email:', email);
       console.log('Password:', password);
+
       // Clear the form fields after successful submission.
-      setUsername('');
       setEmail('');
       setPassword('');
     }
-  };
-
-  const handleToggle = () => {
-    setError(''); // Clear any previous error when toggling
-    setIsLogin((prevState) => !prevState); // Toggle between login and signup
   };
 
   return (
     <div>
       <br />
       <br />
-      <div id="signup_form">
-        <h1 className="h3 mb-3 font-weight-normal">{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <div id="login_form">
+        <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
         <br />
         <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="username"
-            value={username}
-            onChange={handleUsernameChange}
-            placeholder="Username"
-            required
-          />
-          <br />
           <input
             type="email"
             name="email"
@@ -85,18 +64,15 @@ const Signup = () => {
           <br />
           {error && <div style={{ color: 'red' }}>{error}</div>}
           <br />
-          <input type="submit" value={isLogin ? 'Login' : 'Sign Up'} className="button1" />
+          <input type="submit" value="Login" className="button1" />
         </form>
         <br />
         <p>
-          {isLogin ? 'Don\'t have an account? ' : 'Already have an account? '}
-          <Link to={isLogin ? '/signup' : '/login'} onClick={handleToggle}>
-            {isLogin ? 'Sign Up' : 'Login'}
-          </Link>
+          Don't have an account? <Link to="/signup">Sign up here</Link>
         </p>
       </div>
     </div>
   );
 };
 
-export default Signup;
+export default Login;
