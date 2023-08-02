f87d745001893b1108ba05995f8ae7fa153c33a7
Cheruiyot Kipkemoi <d.cheru112@gmail.com>
2023-08-02 11:36:50 +0300

    Styling for Destinations

--- src/pages/destination/Destination.jsx
+++ src/pages/destination/Destination.jsx
@@ -1,30 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Masonry from 'react-masonry-css';
 import { Link } from 'react-router-dom';
+import './Destination.css';
+
+import mockDestinations from './mockData'; // Import the mock data
 
 const Destination = () => {
-  const [destinations, setDestinations] = useState([]);
-
-  useEffect(() => {
-    // Fetch data from the API endpoint
-    fetch('/destinations')  // Update the API endpoint
-      .then(response => response.json())
-      .then(data => setDestinations(data))
-      .catch(error => console.error(error));
-  }, []);
-
   return (
-    <div className="destination-page">
-      <h2 className="destination-title">Destinations</h2>
+    <div className="destination-container">
+      <h1 className="destination-title">Destinations</h1>
       <p className="destination-description">
-      Embark on a journey of a lifetime with our premier travel agency.
-        Discover the world's most captivating destinations and immerse yourself
-        in unique cultures, breathtaking landscapes, and unforgettable experiences.
-        Choose among an extensive selection of alluring locations, each offering
-        its own blend of charm and enchantment. Your extraordinary journey starts here!
+        Embark on a journey of a lifetime with our premier travel agency. Discover the world's most captivating destinations and immerse yourself in unique cultures, breathtaking landscapes, and unforgettable experiences. Choose among an extensive selection of alluring locations, each offering its own blend of charm and enchantment. Your extraordinary journey starts here!
       </p>
-      <Masonry breakpointCols={3} className="destination-masonry" columnClassName="destination-masonry-column">
-        {destinations.map(destination => (
+      <Masonry breakpointCols={3} className="masonry-grid" columnClassName="masonry-grid-column">
+        {mockDestinations.map(destination => (
           <Link to={`/destination/${destination.id}`} key={destination.id} className="destination-link">
             <div className="destination-item">
               <img src={destination.image_url} alt={destination.location} className="destination-image" />
--- src/pages/destination/mockData.js
+++ src/pages/destination/mockData.js
@@ -1,17 +1,57 @@
-// mockData.js
-
 const mockDestinations = [
     {
       id: 1,
-      location: 'Mock Location 1',
-      country: 'Mock Country 1',
-      image_url: 'https://example.com/mock-image-1.jpg', // Replace with actual image URLs
+      location: 'Nakuru',
+      country: 'Kenya',
+      image_url: 'https://destinationskenya.com/wp-content/uploads/2021/07/Nakuru-kenya.jpg',
     },
     {
       id: 2,
-      location: 'Mock Location 2',
-      country: 'Mock Country 2',
-      image_url: 'https://example.com/mock-image-2.jpg',
+      location: 'Dubai',
+      country: 'United Arab Emirates',
+      image_url: 'https://images.unsplash.com/flagged/photo-1559717865-a99cac1c95d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZHViYWl8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      id: 3,
+      location: 'Mount Kenya',
+      country: 'Kenya',
+      image_url: 'https://tour.epesicloud.com/app/storage/uploads/37/1686061247_19_2_Giraffes_At_Mount_Kenya_With_Bonfire_Adventures.jpg',
+    },
+    {
+      id: 4,
+      location: 'South Africa',
+      country: 'South Africa',
+      image_url: 'https://tour.epesicloud.com/app/storage/uploads/37/1671095042_80_table-mountain-cable-car.jpg',
+    },
+    {
+      id: 5,
+      location: 'Diani',
+      country: 'Kenya',
+      image_url: 'https://tour.epesicloud.com/app/storage/uploads/37/1683886539_94_Mombasa_North_Coast_Pride_Inn_Paradise.png',
+    },
+    {
+      id: 6,
+      location: 'Seychelles',
+      country: 'Seychelles',
+      image_url: 'https://tour.epesicloud.com/app/storage/uploads/37/1670405048_56_SEZLC__Feet_In_Sand_Dining.jpg',
+    },
+    {
+      id: 7,
+      location: 'Amboseli',
+      country: 'Kenya',
+      image_url: 'https://tour.epesicloud.com/app/storage/uploads/37/1686048888_3_Elephants_At_Amboseli_National_Park_With_Bonfire_Adventures.jpg',
+    },
+    {
+      id: 8,
+      location: 'Mauritius',
+      country: 'Mauritius',
+      image_url: 'https://tour.epesicloud.com/app/storage/uploads/37/1675755043_72_pool.jpg',
+    },
+    {
+      id: 9,
+      location: 'Maasai Mara',
+      country: 'Kenya',
+      image_url: 'https://images.unsplash.com/photo-1547970810-dc1eac37d174?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHdpbGRsaWZlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
     },
     // Add more mock destinations as needed
   ];
