0b9ad1bb1fe35fdace745b5fac555107b1c6d8a2
wanja <114565394+WanjaNjunge@users.noreply.github.com>
2023-08-07 19:31:13 +0300

    Integrated destinations and packages get requests

--- src/pages/destination/Destination.jsx
+++ src/pages/destination/Destination.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Masonry from 'react-masonry-css';
 import { Link } from 'react-router-dom';
 import './Destination.css';
 
-import mockDestinations from './mockData'; // Import the mock data
-
 const Destination = () => {
+  const [destinations, setDestinations] = useState([]);
+
+  useEffect(() => {
+    // Fetch destinations data from your backend API
+    fetch('/destinations') // Replace with your API endpoint
+      .then(response => response.json())
+      .then(data => {
+        setDestinations(data); // Assuming the data contains the destinations array
+      })
+      .catch(error => {
+        console.error('Error fetching destinations:', error);
+      });
+  }, []);
+
   return (
     <div className="destination-container">
       <h1 className="destination-title">Destinations</h1>
       <p className="destination-description">
-        Embark on a journey of a lifetime with our premier travel agency. Discover the world's most captivating destinations and immerse yourself in unique cultures, breathtaking landscapes, and unforgettable experiences. Choose among an extensive selection of alluring locations, each offering its own blend of charm and enchantment. Your extraordinary journey starts here!
+        {/* Your description here */}
       </p>
       <Masonry breakpointCols={3} className="masonry-grid" columnClassName="masonry-grid-column">
-        {mockDestinations.map(destination => (
+        {destinations.map(destination => (
           <Link to={`/destination/${destination.id}`} key={destination.id} className="destination-link">
             <div className="destination-item">
               <img src={destination.image_url} alt={destination.location} className="destination-image" />
--- src/pages/packages/Packages.jsx
+++ src/pages/packages/Packages.jsx
@@ -1,35 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // Step 1: Import Link
-import './Packages.css'; // Import the CSS file
-import cardData from './cardData';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './Packages.css';
 
 const Packages = () => {
+  const [travelPackages, setTravelPackages] = useState([]);
+
+  useEffect(() => {
+    fetch('/travel_packages')
+      .then(response => response.json())
+      .then(data => setTravelPackages(data))
+      .catch(error => console.error('Error fetching travel packages:', error));
+  }, []);
 
   return (
     <div>
-      {cardData.map((cardData) => (
-        <div className="card" key={cardData.id}>
-          <h2>{cardData.name}</h2>
+      {travelPackages.map(travelPackage => (
+        <div className="card" key={travelPackage.id}>
+          <h2>{travelPackage.name}</h2>
           <div className="image-container">
-            {cardData.image.map((img, index) => (
-              <img key={index} src={img} alt={`Image ${index + 1}`} />
-            ))}
+            <img src={travelPackage.image_url} alt={`Image for ${travelPackage.name}`} />
           </div>
-
-          <p>Location: {cardData.location}</p>
-          <p>{cardData.description}</p>
-          <p>Duration: {cardData.duration}</p>
-          <p>Price: ${cardData.price}</p>
-          <ul>
-            <h3>Itinerary</h3>
-            {cardData.itinerary.map((day, index) => (
-              <li key={index}>{day}</li>
-            ))}
-          </ul>
-
-          <p>Rating: {cardData.rating}</p>
-
-          {/* Booking Button */}
+          <p>Location: {travelPackage.destination.location}</p>
+          <p>{travelPackage.description}</p>
+          <p>Price: ${travelPackage.pricing}</p>
+          {Array.isArray(travelPackage.itinerary) ? (
+            <ul>
+              <h3>Itinerary</h3>
+              {travelPackage.itinerary.map((day, index) => (
+                <li key={index}>{day}</li>
+              ))}
+            </ul>
+          ) : (
+            <p>Itinerary: {travelPackage.itinerary}</p>
+          )}
+          <p>Booking Status: {travelPackage.booking_status}</p>
           <Link to="/bookingform" className="booking-button">Book Now</Link>
         </div>
       ))}
