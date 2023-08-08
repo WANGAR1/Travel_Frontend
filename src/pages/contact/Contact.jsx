3ed24ee4daa091d21207fb2fd6e3b7966423bf01
Kipkemoi Cheruiyot D <d.cheru112@gmail.com>
2023-08-08 17:51:32 +0300

    Update Contact.jsx to handle submission

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './Contact.css';
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+  const [successMessage, setSuccessMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios.post('/contacts', { contact: formData })
+      .then((response) => {
+        console.log(response.data.message);
+        setSuccessMessage('We have received your message. Our dedicated team will reach out to you in the shortest time!');
+        setErrorMessage('');
+        
+        setFormData({
+          name: '',
+          email: '',
+          message: '',
+        });
+      })
+      .catch((error) => {
+        console.error(error.response.data.errors);
+        setErrorMessage('Error submitting form. Please try again later.');
+        setSuccessMessage('');
+      });
+  };
+
   return (
-    
     <section className="p-6 text-gray-900">
-      <form noValidate className="container w-full max-w-xl p-8 mx-auto space-y-6 rounded-md shadow bg-white">
+      <form onSubmit={handleSubmit} noValidate className="container w-full max-w-xl p-8 mx-auto space-y-6 rounded-md shadow bg-white">
         <h2 className="w-full text-3xl font-bold leading">Contact us</h2>
+        {successMessage && <p className="text-green-500">{successMessage}</p>}
+        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
         <div>
           <label htmlFor="name" className="block mb-1 ml-1">
             Name
@@ -14,6 +51,9 @@ const Contact = () => {
           <input
             id="name"
             type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
             placeholder="Your name"
             required
             className="block w-full p-2 rounded focus:outline-none focus:ring focus:border-indigo-500 bg-gray-100"
@@ -30,6 +70,9 @@ const Contact = () => {
           <input
             id="email"
             type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
             placeholder="Your email"
             required
             className="block w-full p-2 rounded focus:outline-none focus:ring focus:border-indigo-500 bg-gray-100"
@@ -41,7 +84,9 @@ const Contact = () => {
           </label>
           <textarea
             id="message"
-            type="text"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
             placeholder="Message..."
             className="block w-full p-2 rounded autoexpand focus:outline-none focus:ring focus:border-indigo-500 bg-gray-100"
           ></textarea>
